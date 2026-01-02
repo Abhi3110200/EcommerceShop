15714bdfeda9b7b641c222f4ed032ab8554be641
Abhijeetdrv <abhijeet.vishwakarma@dwebbox.com>
2026-01-03 01:04:27 +0530

    Add: Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import path from "path";
+import cors from "cors";
+import { ENV } from "./config/env.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const __dirname = path.resolve();
+
+ 
+const PORT = ENV.PORT;
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+    
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
