d85d50c66f4eb4b977b31a0c467b3a3b1ed0d325
Abhijeetdrv <abhijeet.vishwakarma@dwebbox.com>
2026-01-03 00:50:48 +0530

    Setup the Frontend Mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text style={{ color: "white" }}>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
